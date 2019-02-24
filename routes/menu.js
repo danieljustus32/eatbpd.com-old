146062b5f66b306a1679c92013b4bf415e8ce2eb
Daniel Justus <daniel.justus32@gmail.com>
2019-02-24 04:49:50 -0600

    Begin setting up routes

--- routes/menu.js
+++ routes/menu.js
@@ -1,11 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-// GET home page. //
-router.get('/', function(req, res, next) {
-  res.render('index');
-});
-
+// GET menu page. //
 router.get('/menu', function(req, res, next) {
   res.render('menu');
 });
