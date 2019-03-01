0d5fcedfb983d5322b66e02603551ebaf7879bce
Daniel Justus <daniel.justus32@gmail.com>
2019-03-01 10:44:54 -0600

    Refactor so that menuBuilder.build() is the function returning menu object

--- modules/menubuilder.js
+++ modules/menubuilder.js
@@ -1,26 +1,21 @@
 module.exports = function menuBuilder(meal, con) {
 	this.meal = meal;
 	this.con = con;
-	var mealCategories = [];
-	this.query = con.query( {
+	var menu = [];
+	this.build = () => con.query( {
 	sql: 'SELECT * FROM ' + meal + ';' 
 	}, 
 	function(err, result) {
     if (err) {
       //Do not throw err as it will crash the server. 
-      console.log(err.code);
+    	console.log(err.code);
     } else {
     	for(var i in result) {
-    		mealCategories.push(result[i]);
-
+    		menu.push(result[i]);
     	}
       //Do anything with the query result
-      console.log(mealCategories);
+      	  console.log(menu);
+      	  return menu;
     } 
 	});
-	this.build = function() {
-		console.log(mealCategories)
-		return mealCategories;
-	};
-
 };
